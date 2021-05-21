bedf2a030d6ff9d7d13d96697d90fe892f2b231d
Leon <CassperZCPH>
2021-05-22 01:36:58 +0300

    create DetectGrid

---
+++ JournalReader/JournalReader/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using WIA;
+using System.Runtime.InteropServices;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace JournalReader
+{
+    public partial class Form1 : Form
+    {
+        JournalRecognizer journalRecognizer = new JournalRecognizer();
+
+        private DeviceInfo AvailableScanner = null;
+
+        private Image<Bgr, byte> inputImage = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DeviceManager deviceManager = new DeviceManager();
+
+                for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+                {
+                    if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                    {
+                        continue;
+                    }
+                    AvailableScanner = deviceManager.DeviceInfos[i];
+                    ListOfScan.Items.Add(AvailableScanner.Properties["Name"].get_Value());
+                    break;
+                }
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnScan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*DeviceManager deviceManager = new DeviceManager();
+
+                DeviceInfo AvailableScanner = null;
+
+                for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+                {
+                    if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                    {
+                        continue;
+                    }
+
+                    AvailableScanner = deviceManager.DeviceInfos[i];
+
+                    break;
+                }*/
+                Device device = AvailableScanner.Connect();
+                Item scanerItem = device.Items[1];
+                IImageFile imgFile = (ImageFile)scanerItem.Transfer(FormatID.wiaFormatJPEG);
+                
+                byte[] imageBites = (byte[])imgFile.FileData.get_BinaryData();
+                MemoryStream ms = new MemoryStream(imageBites);
+                pictureBox1.Image = Image.FromStream(ms);
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                    inputImage = new Image<Bgr, byte>(openFileDialog1.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("Файл не выбран", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный формат файла", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (inputImage == null)
+            {
+                MessageBox.Show("Изображение не выбрано", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                journalRecognizer.DetectGrid(inputImage, out MemoryStream streamImage);
+                pictureBox1.Image = Image.FromStream(streamImage);
+            }
+        }
+    }
+}
+
+/* для сохранения
+string path = @"C:\Users\Каспер\Desktop\Дэплом\Сканы";
+string fileName = @"\ScanImg1.jpg";
+
+if (!Directory.Exists(path))
+{
+    Directory.CreateDirectory(path);
+}
+
+path += fileName;
+
+if (File.Exists(path))
+{
+    int index = path.LastIndexOf("Img1") + 3;
+    int numberOfFile = Convert.ToInt32(path[index]);
+    numberOfFile += 1;
+    path.Replace("Img1", "Img" + Convert.ToChar(numberOfFile));
+}
+*/
---
+++ JournalReader/JournalReader/JournalRecognizer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace JournalReader
+{
+    class JournalRecognizer
+    {
+        //private LineSegment2D[] lines;
+        private List<LineSegment2D> lineList = new List<LineSegment2D>();
+
+        public void DetectGrid(Image<Bgr, byte> img, out MemoryStream streamImage)
+        {
+            Image<Gray, byte> edge = new Image<Gray, byte>(img.Width, img.Height, new Gray(0));
+
+            CvInvoke.Canny(img, edge, 50, 150);
+
+            //CvInvoke.CvtColor(edge, copyedge, Emgu.CV.CvEnum.ColorConversion.Gray2Bgr);
+
+            using (VectorOfPointF vector = new VectorOfPointF())
+            {
+                CvInvoke.HoughLines(edge, vector, 1, Math.PI / 180, 230);
+
+                for (int i = 0; i < vector.Size; i++)
+                {
+                    float rho = vector[i].X;
+                    float theta = vector[i].Y;
+                    Point pt1 = new Point();
+                    Point pt2 = new Point();
+                    double a = Math.Cos(theta);
+                    double b = Math.Sin(theta);
+                    double x0 = a * rho;
+                    double y0 = b * rho;
+                    pt1.X = (int)Math.Round(x0 + img.Width * (-b));
+                    pt1.Y = (int)Math.Round(y0 + img.Height * a);
+                    pt2.X = (int)Math.Round(x0 - img.Width * (-b));
+                    pt2.Y = (int)Math.Round(y0 - img.Height * a);
+                    bool condAlternationLines = CheckAlternationLines(new LineSegment2D(pt1, pt2));
+                    bool condDiagonal = Math.Abs(pt1.X - pt2.X) > 10 && Math.Abs(pt1.Y - pt2.Y) > 10;
+                    bool condLeftEdge = (pt1.X > 0 && pt1.X < img.Width*0.33) && !(pt1.X > img.Width*0.15 && pt1.X < img.Width*0.16);
+                    bool condTopEdge = pt1.Y > 0 && pt1.Y < img.Height*0.1;
+                    bool condBottomEdge = pt1.Y > img.Height * 0.92 && pt1.Y < img.Height;
+                    if (condDiagonal || condLeftEdge || condTopEdge || condBottomEdge) continue;
+                    if (condAlternationLines) continue;
+
+                    lineList.Add(new LineSegment2D(pt1, pt2));
+                    CvInvoke.Line(img, pt1, pt2, new Bgr(Color.Red).MCvScalar, 3, Emgu.CV.CvEnum.LineType.AntiAlias);
+                }
+                //lines = lineList.ToArray();
+                lineList.Clear();
+            }
+            CvInvoke.Imwrite("imageGrid.jpg", img);
+            streamImage = new MemoryStream(img.ToJpegData());
+        }
+
+        private bool CheckAlternationLines(LineSegment2D lineSegment)
+        {
+            for (int i = 0; i < lineList.Count; i++)
+            {
+                bool condX = Math.Abs(lineSegment.P1.X - lineList[i].P1.X) < 40 && Math.Abs(lineSegment.P2.X - lineList[i].P2.X) < 40;
+                bool condY = Math.Abs(lineSegment.P1.Y - lineList[i].P1.Y) < 40 && Math.Abs(lineSegment.P2.Y - lineList[i].P2.Y) < 40;
+                if (condX && condY) return true;
+            }
+            return false;
+        }
+    }
+}
